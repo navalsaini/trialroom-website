5630096dc7abf183bb8ed739b445fba60ad736b5
navalsaini <navalnovel@gmail.com>
2015-04-13 22:19:34 +0530

    latest updates

--- scripts/directives/ff-imagepoll.js
+++ scripts/directives/ff-imagepoll.js
@@ -12,6 +12,7 @@ angular
         var ion_correct_answer = "icon ion-checkmark-round ff-correct";
         var isAnswered = false;
         var correctMessage = "You caught us";
+        var wrongMessage = "Flixstock";
 
         var data = JSON.parse($attr.data);
         var onPoll = $scope[$attr.onPoll];
@@ -28,23 +29,23 @@ angular
             return;
           }
           isAnswered = true;
+
+          $scope.overlayLeftMessage = "";
+          $scope.overlayRightMessage = "";
+          $scope.overlayRightIconCls = "";
+          $scope.overlayLeftIconCls = "";
+
           var isCorrect = (data.isLeft === isLeft);
           if(data.isLeft){
             $scope.overlayLeftIconCls = ion_correct_answer;
-            if(isCorrect){
-              $scope.overlayLeftMessage = correctMessage;
-            }
-            $scope.overlayRightIconCls = "";
-            $scope.overlayRightMessage = "";
+            $scope.overlayLeftMessage = (isCorrect) ? correctMessage : wrongMessage;
           }else{
-            $scope.overlayLeftIconCls = "";
-            $scope.overlayLeftMessage = "";
             $scope.overlayRightIconCls = ion_correct_answer;
-            if(isCorrect){
-              $scope.overlayRightMessage = correctMessage;
-            }
+            $scope.overlayRightMessage = (isCorrect) ? correctMessage : wrongMessage;
           }
+
           $element.find('.imagepoll-overlay').css('opacity', '1');
+
           if(onPoll){
             onPoll(data.idx, isCorrect, false);
           }
